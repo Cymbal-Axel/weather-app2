b0f5cc2b6fadecad3dfa91ba2f6e3a620407c84b
Axel <cymbalaxel@gmail.com>
2023-11-03 15:06:54 -0300

    se agrego una variable que contiene la api call

---
+++ src/components/WeatherPanel.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+const WeatherPanel = () => {
+ 
+    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?appid=2388c2c8d7a7dfad1732511f225a207c&lang=es";
+    let cityUrl = "&q="
+}
