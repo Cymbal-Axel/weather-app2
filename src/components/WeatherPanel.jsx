4531975542c452223320a9f87784a4aeb618e077
Axel <cymbalaxel@gmail.com>
2023-11-08 17:11:02 -0300

    se agrego una funcion asincrona, con un fetch para el weather y otro para el forecast

--- src/components/WeatherPanel.jsx
+++ src/components/WeatherPanel.jsx
@@ -9,4 +9,48 @@ const WeatherPanel = () => {
 
     const [weather, setWeather] = useState([]);
     const [forecast, setForecast] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [show, setShow] = useState(false);
+    const [location, setLocation] = useState("");
+
+    const getLocation = async(loc) => {
+        setLoading(true);
+        setLocation(loc);
+
+           //weather
+            urlWeather = urlWeather + cityUrl + loc;
+
+            await fetch(urlWeather).then((response) => {
+                if(!response.ok) throw {response}
+                return response.json();
+            }).then((weatherData) => {
+                setWeather(weatherData);
+            }).catch(error => {
+                console.log(error);
+                setLoading(false); 
+                setShow(false)
+            });
+
+            //forecast
+            urlForecast = urlForecast + cityUrl + loc;
+            await fetch(urlForecast).then((response)=> {
+                if(!response.ok) throw{response}
+                return response.json();
+            }).then((forecastData)=> {
+                console.log(forecastData);
+                setForecast(forecastData);
+
+                setLoading(false);
+                setShow(true);
+
+            }).catch(error =>{
+                console.log(error);
+                setLoading(false);
+                setShow(false);
+            })
+        }
+
+        return(
+            
+        )
 }
