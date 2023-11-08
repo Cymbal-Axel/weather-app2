a47279c0eb0941437675e9fd8d1020f467db6e3d
Axel <cymbalaxel@gmail.com>
2023-11-08 15:54:25 -0300

    se agrego un pedido del pronostico, y dos estados

--- src/components/WeatherPanel.jsx
+++ src/components/WeatherPanel.jsx
@@ -3,5 +3,10 @@ import React, {useState} from 'react';
 const WeatherPanel = () => {
  
     let urlWeather = "https://api.openweathermap.org/data/2.5/weather?appid=2388c2c8d7a7dfad1732511f225a207c&lang=es";
-    let cityUrl = "&q="
+    let cityUrl = "&q=";
+
+    let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=2388c2c8d7a7dfad1732511f225a207c&lang=es";
+
+    const [weather, setWeather] = useState([]);
+    const [forecast, setForecast] = useState([]);
 }
