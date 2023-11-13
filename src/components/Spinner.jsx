237a87866be807155c23b90e743386b499cd4cf7
Axel <cymbalaxel@gmail.com>
2023-11-13 15:49:36 -0300

    el componente va a contener el icono de loading

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets-css/Spinner.css';
+
+const Spinner = () => {
+    return(
+        <div class="lds-spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+
+}
+
+export default Spinner;
