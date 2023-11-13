c30bd61e9dc975f43747fc6cc541ec53482a67f7
Axel <cymbalaxel@gmail.com>
2023-11-13 16:20:04 -0300

    se trajeron las props pasadas en el componente WeatherPanel, y s se utilizo un operador ternario para mostrar datos en caso de que se encuentren o un mensaje de "sin datos"

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,33 @@ const Card = ({showData, loadingData, weather, forecast}) => {
         return <Spinner />;
     }
     return(
-        <h1></h1>
+        <div className='mt-5'>
+
+            {
+                showData === true ? (
+
+                    <div className='container'>
+                        <div className='card mb-3 mx-auto bg-dark text-light'>
+                            <div className='row g-0'>
+                                <div className='col-md-4'>
+
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className='card-body text-start mt-2'></div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                ):(
+                    <h2 className='text-light'>Sin Datos</h2>
+                )
+            }
+
+
+        </div>
     );
 }
 
