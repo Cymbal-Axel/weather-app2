a3bb43b19442468c0f64b5e110eca2e9134f69e9
Axel <cymbalaxel@gmail.com>
2023-11-13 18:00:19 -0300

    se agrego una imagen en Card donde se mostrarian los datos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,7 +16,7 @@ const Card = ({showData, loadingData, weather, forecast}) => {
                         <div className='card mb-3 mx-auto bg-dark text-light'>
                             <div className='row g-0'>
                                 <div className='col-md-4'>
-
+                                    <img src='https://images.pexels.com/photos/2422588/pexels-photo-2422588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='img-fluid rounded-start' alt='..'/>
                                 </div>
                                 <div className='col-md-8'>
                                     <div className='card-body text-start mt-2'></div>
